d5d59e430b6eb5108c9ab0beb9aae79760178166
Bibek Kumar Singh <61410793+vivekvikash98322@users.noreply.github.com>
2023-12-11 05:32:30 +0000

    Added number of sun array program

--- app/src/main/java/array_strings/arraysProbems/SlidingWindow/NumberOfSubArrayWithProductGiven.java
+++ app/src/main/java/array_strings/arraysProbems/SlidingWindow/NumberOfSubArrayWithProductGiven.java
@@ -11,6 +11,30 @@ public class NumberOfSubArrayWithProductGiven {
     public static void main(String[] args) {
         
         int array [] = {10,5,2,6};
+        int k = 100;
+        
+        int start = 0;
+        int end = 0;
+
+        int prod = 1;
+        int ans = 0 ;
+
+        while(end < array.length){
+
+            prod *= array[end];
+
+            while(prod >= k){
+
+                prod /= array[start];
+
+                start++;
+            }
+
+            ans += end - start + 1;
+            end++;
+        }
+
+        System.err.println(ans);
         
     }
 }
